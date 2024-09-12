5c77cca4eae9522551b4d470725aafae687438cb
EricRhee <123757258+EricRhee@users.noreply.github.com>
2024-09-12 17:20:23 -0400

    flash
    setting up flash

--- back/index.js
+++ back/index.js
@@ -2,10 +2,31 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
 const UserModel = require('./models/Users')
 
 const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(flash());
+
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use(express.json());
@@ -17,10 +38,18 @@ db.once('open', ()=> {
     console.log("Mongodb connection successful")
 })
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    console.log("got throught")
+    next();
+});
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    //res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.render('index', { message : req.flash('message')})
 });
+
 
 app.get("/getUsers", (req, res) => {
     UserModel.find({}).then(function(users){
@@ -44,8 +73,9 @@ app.post("/login", async function(req, res) {
         if (user) {
             const result = req.body.password == user.password;
             if (result) {
-
-                res.send('successful');
+                req.flash('message', 'Login Successful')
+                console.log('Flash message set: Login Successful');
+                res.redirect("/")
                 
             } else  {
                 res.status(400).json({ error: "password doesn't match" });
